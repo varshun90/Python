8ec4b61b337e55b5d193324e0aa6583dc7bcb959
Varshu <varshini.nimmagadda@gmail.com>
2016-07-31 17:00:33 -0700

    door_mat

---
+++ hackerrank/designer_door_mat.py
@@ -0,0 +1,36 @@
+N = int(input("Enter no.of rows:"))
+M = int(input("Enter no.of coloums:"))
+
+def main():
+
+    # Print top row
+    for i in range(0, int((N) / 2)):
+        no_of_patterns = i + i + 1
+        no_of_dashes = int((M - (no_of_patterns * 3)) / 2)
+        print_string(no_of_dashes, "-")
+        print_string(no_of_patterns, ".|.")
+        print_string(no_of_dashes, "-")
+        print("")
+
+    # print welcome
+    no_of_welcome_dashes = int((M - 7) / 2)
+    print_string(no_of_welcome_dashes, "-")
+    print("WELCOME", end="")
+    print_string(no_of_welcome_dashes, "-")
+    print("")
+
+    # print bottom row
+    for i in range(int((N) / 2)+1, N):
+        patterns = (2 * (N - i)) - 1
+        dashes = int((M - (patterns * 3)) / 2)
+        print_string(dashes, "-")
+        print_string(patterns, ".|.")
+        print_string(dashes, "-")
+        print("")
+
+def print_string(no_of_times, string_to_print):
+    for j in range(0, no_of_times):
+        print(string_to_print, end="")
+
+
+main()
